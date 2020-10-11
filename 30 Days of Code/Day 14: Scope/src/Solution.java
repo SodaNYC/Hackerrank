16c89963e7ec72a48a2c1f5c48f6d6c6bd7c9b56
Hao Dong <caomuzhongtu@hotmail.com>
2020-10-11 02:19:21 -0400

    Day 14 & got 3 stars, yay

---
+++ 30 Days of Code/Day 14: Scope/src/Solution.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+    public int max = 1;
+    public int min = 10;
+
+    // Add your code here
+    Difference(int[] elements){
+        this.elements = elements;
+    }
+
+    public int computeDifference(){
+        for(int i = 0 ; i < elements.length; i++){
+            if(elements[i]>max){
+                max = elements[i];
+            }
+
+            if(elements[i]<min){
+                min = elements[i];
+            }
+        }
+        maximumDifference = Math.abs(max-min);
+        return maximumDifference;
+    }
+
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
